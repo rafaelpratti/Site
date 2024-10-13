029d6e3fc8d5cd22f3b458d84d9b39c7062e1cea
ksolegario <kalebe.olegario1850@gmail.com>
2024-10-13 19:23:16 -0300

    a

--- js/cadastrar.js
+++ js/cadastrar.js
@@ -28,6 +28,54 @@ function validarFormSenha(event) {
 // Adicionando o evento de submit ao formulário
 document.getElementById("formcadastro").addEventListener("submit", validarFormSenha);
 
+// Definindo valores padrões
+let fontSize = 12;
+const body = document.body;
+
+// Funções para mudar o tamanho da fonte
+function increaseFont() {
+    fontSize += 2;
+    body.style.fontSize = fontSize + 'px';
+    saveSettings();
+}
+
+function decreaseFont() {
+    fontSize -= 2;
+    body.style.fontSize = fontSize + 'px';
+    saveSettings();
+}
+
+function resetFontAndContrast() {
+    fontSize = 12;
+    body.style.fontSize = '16px';
+    body.className = ''; // Remove todas as classes de contraste
+    saveSettings(); // Salva o estado padrão no localStorage
+}
+
+// Funções para mudar o contraste
+function setContrast(className) {
+    body.className = className;
+    saveSettings();
+}
+
+// Função para salvar as configurações no localStorage
+function saveSettings() {
+    localStorage.setItem('fontSize', fontSize);
+    localStorage.setItem('contrast', body.className);
+}
+
+// Função para carregar as configurações ao carregar a página
+function loadSettings() {
+    if (localStorage.getItem('fontSize')) {
+        fontSize = parseInt(localStorage.getItem('fontSize'));
+        body.style.fontSize = fontSize + 'px';
+    }
+
+    if (localStorage.getItem('contrast')) {
+        body.className = localStorage.getItem('contrast');
+    }
+}
+
 // Adicionando eventos aos botões
 document.getElementById('increase-font').addEventListener('click', increaseFont);
 document.getElementById('decrease-font').addEventListener('click', decreaseFont);

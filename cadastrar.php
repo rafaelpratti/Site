e62820ec2c88a60b4579bf65c5a802dc58a8088b
ksolegario <kalebe.olegario1850@gmail.com>
2024-10-12 22:33:19 -0300

    a

--- cadastrar.php
+++ cadastrar.php
@@ -21,6 +21,24 @@
         </nav>
     </header>
 
+    <aside id="accessibility-bar">
+        <div class="accessibility-box">
+            <h2>Acessibilidade</h2>
+            <div class="font-size-control">
+                <button id="decrease-font" class="square-button">A-</button>
+                <button id="default-font" class="square-button" disabled>A</button>
+                <button id="increase-font" class="square-button">A+</button>
+            </div>
+            <div class="contrast-control">
+                <button id="contrast-normal" class="square-button">A</button>
+                <button id="contrast-yellow" class="square-button" style="background-color: yellow;">A</button>
+                <button id="contrast-blue" class="square-button" style="background-color: blue; color: white;">A</button>
+                <button id="contrast-black" class="square-button" style="background-color: black; color: yellow;">A</button>
+            </div>
+            <button id="reset" class="reset-button">Redefinir</button>
+        </div>
+    </aside>
+
     <section class="sec1">
         <img src="img/cadastro_img.png">
         <div>

0f01af67b6be942ec4a5dc7a0e13bc33731f0bbd
ksolegario <kalebe.olegario1850@gmail.com>
2024-11-29 15:20:12 -0300

    a

--- download.php
+++ download.php
@@ -1,41 +1,36 @@
 <?php include 'cabecalho_footer.php'; ?>
 
-    <section class="sec1">
+<section class="sec1">
+    <div class="sec1-div2">
+        Faça o download da versão mais recente do CodeQuest clicando no botão abaixo.
+    </div>
 
-        <div class="sec1-div2">
-            Faça o download da versão mais recente do TRILINGO clicando no botão abaixo .
+    <a href="https://drive.google.com/drive/folders/1Ea8yMIcgQc002MJvvEnp9RWXRN3bOOcZ?usp=drive_link">
+        <div class="sec1-div1 download-button">
+            <img src="img/download2.png" alt="download">
+            <span>Baixe agora e comece sua jornada!</span>
         </div>
+    </a>
+</section>
 
-        <a href="https://drive.google.com/drive/folders/1Ea8yMIcgQc002MJvvEnp9RWXRN3bOOcZ?usp=drive_link">
-            <div class="sec1-div1">
-                <img src="img/download2.png" alt="download">
-            </div>
-        </a>
+<section>
+    <div id="acbox" class="accessibility-box">
+        <div class="font-size-control">
+            <button id="decrease-font" class="square-button">A-</button>
+            <button id="increase-font" class="square-button">A+</button>
+        </div>
+        <div class="contrast-control">
+            <button id="contrast-normal" class="square-button">A</button>
+            <button id="contrast-yellow" class="square-button" style="background-color: yellow;">A</button>
+            <button id="contrast-blue" class="square-button" style="background-color: blue; color: white;">A</button>
+            <button id="contrast-black" class="square-button" style="background-color: black; color: yellow;">A</button>
+        </div>
+        <button id="reset" class="reset-button">Redefinir</button>
+    </div>
+    <button type="button" class="collapsible" id="collapsible">Acessibilidade</button>
+</section>
 
-        <h1>DOWNLOAD</h1>
+<script src="js/download.js"></script>
 
-    </section>
-
-    <section>
-            <div id="acbox" class="accessibility-box">
-        
-                <div class="font-size-control">
-                    <button id="decrease-font" class="square-button">A-</button>
-                    <button id="increase-font" class="square-button">A+</button>
-                </div>
-                <div class="contrast-control">
-                    <button id="contrast-normal" class="square-button">A</button>
-                    <button id="contrast-yellow" class="square-button" style="background-color: yellow;">A</button>
-                    <button id="contrast-blue" class="square-button" style="background-color: blue; color: white;">A</button>
-                    <button id="contrast-black" class="square-button" style="background-color: black; color: yellow;">A</button>
-                </div>
-                <button id="reset" class="reset-button">Redefinir</button>
-            </div>
-        <button type="button" class="collapsible" id="collapsible">Acessibilidade</button>
-    </section>
-
-    <script src="js/download.js"></script>
-    
 </body>
-
 </html>
